f74dc136e77b5f9800511953168c0baa5ef5d403
franklam2008 <franklam2008@gmail.com>
2020-03-11 12:51:16 -0400

    fix accounts responsive

--- src/view/Dashboard/components/Accounts/Accounts.js
+++ src/view/Dashboard/components/Accounts/Accounts.js
@@ -22,7 +22,31 @@ const Accounts = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+      
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   const classes = useStyles();
 
