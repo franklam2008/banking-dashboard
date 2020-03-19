94e612b23f2e68347b2288b521eb4d4772bf8772
franklam2008 <franklam2008@gmail.com>
2020-03-19 14:06:32 -0400

    update style

--- src/view/Dashboard/components/Accounts/Accounts.js
+++ src/view/Dashboard/components/Accounts/Accounts.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Slider from "react-slick";
+import styled from "styled-components";
 import { makeStyles } from "@material-ui/styles";
+const ImgCon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+  max-width: 250px;
+  background-color: white;
+  margin-right: 20px;
+  border-radius: 10px;
+  margin: 0 auto;
+`;
+
 const useStyles = makeStyles(() => ({
   img: {
     width: 100
-  },
-  imgCon: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height:150,
-    background:'white',
-    marginRight:20,
-    borderRadius:10,
-    outline:'none'
   }
 }));
 const Accounts = () => {
@@ -28,15 +31,14 @@ const Accounts = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 3,
-      
+          slidesToScroll: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -55,33 +57,33 @@ const Accounts = () => {
       <h3>Accounts</h3>
       <Slider {...settings}>
         <div>
-          <h3 className={classes.imgCon}>
+          <ImgCon>
             <img className={classes.img} src="images/logos/rh.png" alt="icon" />
-          </h3>
+          </ImgCon>
         </div>
         <div>
-          <h3 className={classes.imgCon}>
+          <ImgCon>
             <img
               className={classes.img}
               src="images/logos/boa.png"
               alt="icon"
             />
-          </h3>
+          </ImgCon>
         </div>
         <div>
-          <h3 className={classes.imgCon}>
+          <ImgCon>
             <img className={classes.img} src="images/logos/wf.png" alt="icon" />
-          </h3>
+          </ImgCon>
         </div>
         <div>
-          <h3 className={classes.imgCon}>
+          <ImgCon>
             <img className={classes.img} src="images/logos/cs.png" alt="icon" />
-          </h3>
+          </ImgCon>
         </div>
         <div>
-          <h3 className={classes.imgCon}>
+          <ImgCon className={classes.imgCon}>
             <img className={classes.img} src="images/logos/m1.png" alt="icon" />
-          </h3>
+          </ImgCon>
         </div>
       </Slider>
     </div>
