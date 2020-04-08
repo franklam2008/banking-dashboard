13bb9db1455656c78427b7a0fdd54fc93f40e9f9
franklam2008 <franklam2008@gmail.com>
2020-04-07 22:08:40 -0400

    store update

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -16,26 +16,28 @@ const initialState = {
       country: "USA",
       state: "Georgia",
       city: "Atlanta",
-      street: "4894  Lakeland Park Drive"
+      street: "4894  Lakeland Park Drive",
     },
     email: "alexa.richardson@devias.io",
     phone: "770-635-2682",
     avatarUrl: "/images/avatars/avatar_2.png",
-    createdAt: 1555016400000
+    createdAt: 1555016400000,
   },
   checking: "5,250",
   credit: "1,600",
   savingProgress: "20,600",
-  invest: "283,200"
+  invest: "283,200",
 };
 function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "INCREMENT":
-      return { ...state,counter: state.counter + 1 };
+      return { ...state, counter: state.counter + 1 };
     case "DECREMENT":
-      return { ...state,counter: state.counter - 1 };
+      return { ...state, counter: state.counter - 1 };
     case "CHECKING":
-      return { ...state, checking: '999' };
+      return { ...state, checking: "999" };
+    case "UPDATE_USER":
+      return { ...state, user: action.payload };
     case "CHECK_STATE":
       console.log("stateNow", state);
       return { ...state };
