2b0e6a545752e83803996a76fec43791321cc2fd
franklam2008 <franklam2008@gmail.com>
2020-04-09 23:35:53 -0400

    chart option

---
+++ src/view/Dashboard/components/SpendingChart/chartOption.js
@@ -0,0 +1,18 @@
+
+
+export const options = {
+  legend: {
+    display: false
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  animation: false,
+  cutoutPercentage: 80,
+  layout: { padding: 0 },
+  tooltips: {
+    enabled: true,
+    mode: 'index',
+    intersect: false,
+    borderWidth: 1,
+  }
+};
