878b7ee03517991ed16a12148cb6c2afb9a004c2
franklam2008 <franklam2008@gmail.com>
2020-04-11 22:36:21 -0400

    test

--- src/view/Test/Test.js
+++ src/view/Test/Test.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { 
+  // useSelector, 
+  useDispatch }
+   from "react-redux";
 
 export default function () {
-  const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   return (
     <div>
       <h2>Example </h2>
-      <h1>{counter}</h1>
+      
       <button onClick={() => dispatch({ type: "CHANGE_CHECKING_NUM" })}>
         CHANGE_CHECKING_NUM{" "}
       </button>
